51e51513f1447a7e313d781dcbb522a4ecb56d09
max <oedinger13@gmail.com>
2025-08-24 15:01:53 +0200

    feat: added toggle termin configuration

--- init.lua
+++ init.lua
@@ -112,3 +112,23 @@ require('gitsigns').setup{
 }
 
 require("bufferline").setup{}
+
+-- configure floating terminal
+require("toggleterm").setup{
+  start_in_insert = true,
+  open_mapping = [[<c-\>]],
+  terminal_mappings = true,
+  direction =  'float'
+}
+
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
