9be3f256d2a13da9fbbbf502e2ae157ef5cbc720
max <oedinger13@gmail.com>
2026-01-18 23:37:43 +0100

    feat: disable yazi on startup

--- lua/config/plugins/yazi.lua
+++ lua/config/plugins/yazi.lua
@@ -1,38 +1,38 @@
 ---@type LazySpec
 return {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    { "nvim-lua/plenary.nvim", lazy = true },
-  },
-  keys = {
-    {
-      "<leader>e",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
-  },
-  ---@type YaziConfig | {}
-  opts = {
-    open_for_directories = true,
-    keymaps = {
-      show_help = "<f1>",
-    },
-  },
-  init = function()
-    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-    -- vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-  end,
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
+	keys = {
+		{
+			"<leader>e",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			"<leader>cw",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"<c-up>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	---@type YaziConfig | {}
+	opts = {
+		-- open_for_directories = true,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	init = function()
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
 }
