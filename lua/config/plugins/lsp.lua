e7bac741340f09cb5f1fa894e8151cabd133628f
max <oedinger13@gmail.com>
2026-01-18 17:10:37 +0100

    feat(lsp): add configuration for more commonly used languages

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -150,48 +150,34 @@ return {
 
 		local servers = {
 			-- clangd = {},
-			-- gopls = {},
+			gopls = {},
 			basedpyright = {},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
+			roslyn = {},
 			ts_ls = {},
-
+			terraformls = {},
+			azure_pipelines_ls = {},
 			lua_ls = {
-				-- cmd = { ... },
-				-- filetypes = { ... },
-				-- capabilities = {},
 				settings = {
 					Lua = {
 						completion = {
 							callSnippet = "Replace",
 						},
-						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-						-- diagnostics = { disable = { 'missing-fields' } },
 					},
 				},
 			},
 		}
 
-		-- Ensure the servers and tools above are installed
-		--
-		-- To check the current status of installed tools and/or manually install
-		-- other tools, you can run
-		--    :Mason
-		--
-		-- You can press `g?` for help in this menu.
-		--
-		-- `mason` had to be setup earlier: to configure its options see the
-		-- `dependencies` table for `nvim-lspconfig` above.
-		--
-		-- You can add other tools here that you want Mason to install
-		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
+		-- add formatters
 		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format Lua code
+			"terraform", -- terraform
+			"stylua", -- lua,
+			"csharpier", -- c#
+			"prettier", --ts, js, css & HTML
+			"black", -- python
+			"ruff", -- python
 		})
+
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
