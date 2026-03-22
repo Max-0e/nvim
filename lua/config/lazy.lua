8a64e3250a2acdcc46050f2a72d7488edb1602f2
Maximilian Oedinger <maximilian.oedinger@sva.de>
2026-03-22 13:53:20 +0100

    feat: add transparency option

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,8 @@ require("lazy").setup({
 			"catppuccin/nvim",
 			config = function()
 				vim.cmd.colorscheme("catppuccin-mocha")
+				-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+				-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 			end,
 		},
 		{ import = "config.plugins" },
